37d694aca1e417b173aeda1aeea07c73f2055b2e
Orlando Verdin <git@orli.land>
2024-02-06 14:54:07 -0700

    Do isograma exercirse

--- sesion-1/isograma/index.js
+++ sesion-1/isograma/index.js
@@ -22,7 +22,38 @@ func("GaTo"); // returns true
 */
 
 function esIsograma(str) {
-  return false;
+  // Comprobar si el string tienen mas de una palabra
+  if (str.split(" ").length > 1) {
+    return false;
+  }
+
+  //Clean up: eliminar espacios al principio y al final
+  str.trim();
+
+  // convertir string a minúsculas y en un array
+  str = [...str.toLowerCase()];
+
+  // remover acentos, verificar si la letra se encuentra en un objeto de vocales con acento
+  // si asi lo es, cambiarla por la vocal sin acento.
+  const vocales = { á: "a", é: "e", í: "i", ó: "o", ú: "u" };
+  str.map((element) => {
+    if (Object.hasOwn(vocales, element)) {
+      return vocales[element];
+    } else {
+      return element;
+    }
+  });
+
+  // Aplicar Set para verificar si hay elementos repetidos
+  const caracteresUnicos = new Set(str);
+
+  // Comparar longitud de string original y el Set para ver si hay diferencia de longitud
+  // Si la hay, quiere decir que hubo elementos repetidos, por lo que no era un isograma
+  if (caracteresUnicos.size === str.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = esIsograma;
